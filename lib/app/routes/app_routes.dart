1e282c4fbb62725ed55ad1039c0ee5f3b3cfc0af
deviaprilia077 <deviaprilia077@gmail.com>
2024-12-15 01:37:18 +0000

    Initial commit

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,20 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const GET_CONNECT = _Paths.GET_CONNECT;
+  static const ARTICLE_DETAIL = _Paths.ARTICLE_DETAIL;
+  static const ARTICLE_DETAIL_WEBVIEW = _Paths.ARTICLE_DETAIL_WEBVIEW;
+  static const CONNECTION = _Paths.CONNECTION;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const GET_CONNECT = '/get-connect';
+  static const ARTICLE_DETAIL = '/article-detail';
+  static const ARTICLE_DETAIL_WEBVIEW = '/article-detail-webview';
+  static const CONNECTION = '/connection';
+}
